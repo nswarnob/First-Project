b2ab29ef59498ab11a0a1cc3984869b904aa7272
Sharif Uddin Arnob <142154073+nswarnob@users.noreply.github.com>
2024-03-07 14:39:39 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let user = prompt("Whats on your mind?");
+let user2 = prompt("Whats on your mouth?");
+
+user = parseInt(user);
+user2 = parseInt(user2);
+
+
+const Result = user + user2 ;
+document.write("Output: " + Result + "<br>");
+
+
+
+
+
+
